01ff8d30082b03d27994d58ce946843eb6388a7e
Hussain <vohrah220@gmail.com>
2023-09-01 00:10:20 +0530

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Hussain on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
